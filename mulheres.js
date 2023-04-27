147849e9c634c52583802b8262e72db4a1861b49
julietedias <juuhdoliveira@hotmail.com>
2023-04-27 20:31:19 -0300

    nova versão do projeto, integrado com o mongodb

--- mulheres.js
+++ mulheres.js
@@ -1,29 +1,83 @@
 const express = require("express");
 const router = express.Router();
+const cors = require("cors"); // permite consumir essa api no front
+
+const conectaBancoDeDados = require("./bancoDeDados");
+conectaBancoDeDados();
+
+const Mulher = require("./mulherModel");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const porta = 3333;
 
-const mulheres = [
-  {
-    nome: "Juliete Dias",
-    imagem: "https://avatars.githubusercontent.com/u/71693559?v=4",
-    minibio: "Desenvolvedora Full-Stack",
-  },
-  {
-    nome: "Iana Chan",
-    imagem: "https://tinyurl.com/yjs48v9s",
-    minibio: "Fundadora da PrograMaria",
-  },
-  {
-    nome: "Nina da Hora",
-    imagem: "https://tinyurl.com/2xzpne3k",
-    minibio: "Hacker antirracista",
-  },
-];
+// GET
+async function mostraMulheres(request, response) {
+  try {
+    const mulheresVindasDoBancoDeDados = await Mulher.find();
 
-function mostraMulheres(request, response) {
-  response.json(mulheres);
+    response.json(mulheresVindasDoBancoDeDados);
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+// POST
+async function criaMulher(request, response) {
+  const novaMulher = new Mulher({
+    nome: request.body.nome,
+    imagem: request.body.imagem,
+    citacao: request.body.citacao,
+    minibio: request.body.minibio,
+  });
+
+  try {
+    const mulherCriada = await novaMulher.save();
+
+    response.status(201).json(mulherCriada);
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+// PATCH
+async function corrigeDadoMulher(request, response) {
+  try {
+    const mulherEncontrada = await Mulher.findById(request.params.id);
+
+    if (request.body.nome) {
+      mulherEncontrada.nome = request.body.nome;
+    }
+
+    if (request.body.imagem) {
+      mulherEncontrada.imagem = request.body.imagem;
+    }
+
+    if (request.body.minibio) {
+      mulherEncontrada.minibio = request.body.minibio;
+    }
+
+    if (request.body.citacao) {
+      mulherEncontrada.citacao = request.body.citacao;
+    }
+
+    const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save();
+    response.json(mulherAtualizadaNoBancoDeDados);
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+// DELETE
+async function deletaDadosMulher(request, response) {
+  try {
+    await Mulher.findByIdAndDelete(request.params.id);
+    response.json({ mensagem: "Mulher deletada com sucesso!" });
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
 function mostraPorta() {
@@ -31,4 +85,7 @@ function mostraPorta() {
 }
 
 app.use(router.get("/mulheres", mostraMulheres));
+app.use(router.post("/mulheres", criaMulher));
+app.use(router.patch("/mulheres/:id", corrigeDadoMulher));
+app.use(router.delete("/mulheres/:id", deletaDadosMulher));
 app.listen(porta, mostraPorta);
